d24887410992abc4f15e6d0c36a43a3aefe12034
grafie <jinpengchen21@163.com>
2022-05-31 16:46:56 +0800

    增加部分测试代码

--- src/test/java/com/grafie/study/test/MyLinkedListTest.java
+++ src/test/java/com/grafie/study/test/MyLinkedListTest.java
@@ -4,6 +4,8 @@ import com.grafie.study.datastructures.list.MyLinkedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * @author grafie grafie.cn
  * @version 1.0.0
@@ -15,10 +17,12 @@ public class MyLinkedListTest {
     @BeforeEach
     void init() {
         linkedList = new MyLinkedList<>();
+        linkedList.add(0);
         linkedList.add(1);
         linkedList.add(2);
         linkedList.add(3);
         linkedList.add(4);
+        linkedList.add(5);
     }
 
     @Test
@@ -34,11 +38,49 @@ public class MyLinkedListTest {
         linkedList.add(3);
         System.out.println(linkedList);
         System.out.println(linkedList.size());
+
+        linkedList.add(1, 9);
+        System.out.println(linkedList);
+        System.out.println(linkedList.size());
     }
 
     @Test
     void removeTest() {
-
+        System.out.println(linkedList);
+        linkedList.add(null);
+        linkedList.add(null);
+        System.out.println(linkedList);
+        System.out.println("size: " + linkedList.size());
+        linkedList.remove(null);
+        System.out.println(linkedList);
+        linkedList.remove(new Integer(2));
+        System.out.println("size: " + linkedList.size());
+        System.out.println(linkedList);
     }
 
+    @Test
+    void getFirstOrGetLastTest() {
+        System.out.println(linkedList);
+        System.out.println(linkedList.getFirst());
+        System.out.println(linkedList.getLast());
+        linkedList.add(9);
+        linkedList.add(10);
+        System.out.println(linkedList);
+        System.out.println(linkedList.getLast());
+    }
+
+    @Test
+    void toArrayTest() {
+        System.out.println(linkedList);
+        System.out.println(Arrays.toString(linkedList.toArray(new Integer[20])));
+        System.out.println(Arrays.toString(linkedList.toArray(new Integer[2])));
+        System.out.println(Arrays.toString(linkedList.toArray()));
+    }
+
+    @Test
+    void clearTest() {
+        System.out.println(linkedList);
+        linkedList.clear();
+        System.out.println(linkedList);
+    }
 }
